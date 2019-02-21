ad7c79f3a79108809a30aed40a6de47ab048a969
Nicholas Hickman <nicholas.hickman@dit1933us.DIGIPEN.EDU>
2019-02-20 16:41:31 -0800

    Add basic flapping/jump script

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    public float jumpForce;
+    private Vector2 movement = new Vector2(1, 1);
+    public KeyCode jumpKey;
+    private Rigidbody2D rigid;
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(this.jumpKey))
+        {
+            rigid.AddForce(new Vector2(0, this.jumpForce));
+        }
+	}
+}
