eaf46c1af29084011df5aa8b6168373f2c57cc0c
Tsering Ngoche <tsering.ngoche@dit1945us.DIGIPEN.EDU>
2019-02-21 15:59:10 -0800

    Basic EnemyAI Setup

---
+++ Assets/EnemyAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+    public Vector2 enemyVelocity = new Vector2(5, 0);
+    public GameObject player;
+    private Rigidbody2D rigid = new Rigidbody2D();
+    // Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.transform.position = enemyVelocity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if(player Y position > enemy position)
+        //  increase vertical position
+        //else
+        //  decrease vertical position
+	}
+}
