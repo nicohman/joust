b2f84dcb56564d1d21ee1f7ff2eb46436fc7f97b
Nicholas Hickman <nicholas.hickman@dit1933us.DIGIPEN.EDU>
2019-02-20 17:28:51 -0800

    Add PlayerController script and basic movement

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour {
     public float jumpForce;
-    private Vector2 movement = new Vector2(1, 1);
-    public KeyCode jumpKey;
     private Rigidbody2D rigid;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         rigid = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetKeyDown(this.jumpKey))
-        {
-            rigid.AddForce(new Vector2(0, this.jumpForce));
-        }
-	}
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    public void jump()
+    {
+        rigid.AddForce(new Vector2(0, this.jumpForce));
+    }
 }
