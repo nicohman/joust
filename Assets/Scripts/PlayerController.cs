1326a6fda95302869a081c51301977389d381071
Nico Hickman <nicohman@demenses.net>
2019-02-25 16:02:57 -0800

    Create new tutorial branch with Tali's initial tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -12,14 +13,11 @@ public class PlayerController : MonoBehaviour {
     public int lives = 3;
     private Rigidbody2D rigid;
     private SpriteRenderer sprite;
-    public float speed = 10;
-    public int going = 1;
-    private float veloc = 0;
-    public float acceleration = 60;
+    public Vector2 speed = new Vector2(10, 10);
     public float immortalTime = 1.0f;
     private RespawnPoint[] respawns;
     private RespawnPoint respawnAt;
-    private new BoxCollider2D collider;
+    private new Collider2D collider;
     public float immortalTimer = 0.0f;
     public int lifePer = 5;
     private int nextLife;
@@ -27,74 +25,93 @@ public class PlayerController : MonoBehaviour {
     private MountAnimations mountAnim;
     public  float respawnTimer = 0.0f;
     public int points = 0;
-    public float bounce = 1.2f;
     public Text lifeText;
     public Text scoreText;
     public GameOver gameOver;
-	// Use this for initialization
-	void Start () {
+    public bool tutorial = false;
+
+    public float flapTimer = 0.0f;
+    public float tutorialTimer = 0.0f;
+
+    // Use this for initialization
+    void Start () {
         this.jumper = GetComponent<Jump>();
         this.rigid = GetComponent<Rigidbody2D>();
         this.sprite = GetComponent<SpriteRenderer>();
         this.mountAnim = GetComponent<MountAnimations>();
-        this.collider = GetComponent<BoxCollider2D>();
+        this.collider = GetComponent<Collider2D>();
         this.respawns = FindObjectsOfType<RespawnPoint>();
         this.nextLife = this.lifePer;
         this.lives--;
         UpdateLifeText();
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (tutorial)
+        {
+            updateTutorial();
+        }
+        else
+        {
+            updateGamemode();
+        }
+    }
+    private void updateTutorial()
+    {
+        this.tutorialTimer += Time.deltaTime;
+
+        // Look at all the commands and do the things that are within the timeframe
+        foreach (var command in commands)
+        {
+            // In timeframe?
+            if ((tutorialTimer >= command.StartTime) && (tutorialTimer <= command.EndTime))
+            {
+                switch (command.Action)
+                {
+                    case TutorialCommand.Actions.PlayerJump:
+
+                        this.flapTimer += Time.deltaTime;
+                        if (this.flapTimer > 0.2f)
+                        {
+                            this.flapTimer -= 0.2f;
+                            this.mountAnim.PlayFlag();
+                            this.jumper.jump();
+                        }
+                        break;
+                }
+            }
+        }
+    }
+    private void updateGamemode()
+    {
         this.immortalTimer -= Time.deltaTime;
         this.respawnTimer -= Time.deltaTime;
+
         if (this.respawnTimer <= 0)
         {
             this.rigid.gravityScale = 1.0f;
             /*if ( this.immortalTimer <= 0f && this.anim.GetBool("Immortal")) {
                 this.anim.SetBool("Immortal", false);
             }*/
-                if (Input.GetKey(this.leftKey))
+            if (Input.GetKey(this.leftKey))
             {
-                going = -1;
-               this.sprite.flipX = true;
-                if ( veloc > -(speed * 1.06))
-                {
-                    veloc -= speed / acceleration;
-                }
+                this.sprite.flipX = true;
+                this.rigid.AddForce(new Vector2(-this.speed.x, 0));
             }
             if (Input.GetKey(this.rightKey))
             {
-                going = 1;
                 this.sprite.flipX = false;
-                if (this.veloc < speed * 1.06 )
-                {
-                    veloc += speed / acceleration;
-                }
+                this.rigid.AddForce(new Vector2(this.speed.x, 0));
             }
-            if (Input.GetKeyDown(this.jumpKey)) 
+            if (Input.GetKeyDown(this.jumpKey))
             {
                 this.mountAnim.PlayFlag();
                 this.jumper.jump();
             }
-            if (veloc < 0)
-            {
-                veloc = Mathf.Max(veloc, -speed);
-            }
-            else
-            {
-                veloc = Mathf.Min(veloc, speed);
-            }
-            this.rigid.velocity = new Vector2(veloc, rigid.velocity.y);
-            UpdateCollider();
-        }
 
-    }
-    private void UpdateCollider()
-    {
-        Vector2 spriteSize = sprite.bounds.size;
-        collider.size = new Vector2(spriteSize.x/2, spriteSize.y/2);
-        collider.offset = new Vector2(0, 0);
+        }
     }
     private void OnGUI()
     {
@@ -132,9 +149,8 @@ public class PlayerController : MonoBehaviour {
             }
             Destroy(collision.gameObject);
         }
-     
 
-        }
+    }
     public void Die()
     {
         if (this.lives > 0)
@@ -156,4 +172,27 @@ public class PlayerController : MonoBehaviour {
         }
     }
     private void AnimationEvent() {}
+
+    private List<TutorialCommand> commands = new List<TutorialCommand>()
+    {
+        new TutorialCommand() { StartTime = 0.0f, EndTime = 3.0f, Action =TutorialCommand.Actions.DisplayText, Text="Welcome to Joust"},
+        new TutorialCommand() { StartTime = 3.0f, EndTime = 6.0f, Action =TutorialCommand.Actions.DisplayText, Text="To Fly"},
+        new TutorialCommand() { StartTime = 2.0f, EndTime = 4.0f, Action =TutorialCommand.Actions.PlayerJump },
+        new TutorialCommand() { StartTime = 6.0f, EndTime = 8.0f, Action =TutorialCommand.Actions.PlayerJump },
+    };
+}
+
+
+public class TutorialCommand
+{
+    public enum Actions
+    {
+        DisplayText,
+        PlayerJump
+    }
+    public float StartTime { get; set; }
+    public float EndTime { get; set; }
+    public Actions Action { get; set; }
+    public string Text { get; set; }
+
 }
--- Assets/SelectMenu.cs
+++ Assets/SelectMenu.cs
@@ -3,19 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
+
 public class SelectMenu : MonoBehaviour {
     public int select = 0;
     public KeyCode upKey;
     public KeyCode downKey;
     public KeyCode pickKey;
     public Text[] picks;
+
+    public DateTime startTime;
 	// Use this for initialization
 	void Start () {
         picks = this.gameObject.GetComponentsInChildren<Text>();
-	}
+        startTime = DateTime.Now;
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        var currentTime = DateTime.Now;
+        var elapsed = currentTime - startTime;
+        if (elapsed.Seconds > 1)
+        {
+            SceneManager.LoadScene("tutorial");
+            return;
+        }
+
         if (Input.GetKeyDown(pickKey))
         {
             SceneManager.LoadScene("arena");
